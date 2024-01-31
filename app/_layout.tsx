758dcf5be3c012174eff6793805834dbea6ba8b8
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-01-31 16:14:21 -0600

    add sign in flow + screen redirect when signing in with apple

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,24 @@
-import { Slot } from 'expo-router';
-import * as SecureStore from 'expo-secure-store';
+// import { Slot, useRouter, useSegments } from 'expo-router';
+// import * as SecureStore from 'expo-secure-store';
+// import { useEffect } from 'react';
 
-// async storage cached token
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      return SecureStore.getItemAsync(key);
-    } catch {
-      return null;
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch {
-      return;
-    }
-  },
-};
+// // async storage cached token
+// const tokenCache = {
+//   async getToken(key: string) {
+//     try {
+//       return SecureStore.getItemAsync(key);
+//     } catch {
+//       return null;
+//     }
+//   },
+//   async saveToken(key: string, value: string) {
+//     try {
+//       return SecureStore.setItemAsync(key, value);
+//     } catch {
+//       return;
+//     }
+//   },
+// };
 
 // const InitialLayout = () => {
 //   const segments = useSegments();
@@ -39,8 +40,47 @@ const tokenCache = {
 //   return <Slot />;
 // };
 
-const RootLayout = () => {
+// const RootLayout = () => {
+//   return <Slot />;
+// };
+
+// export default RootLayout;
+
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import { AuthProvider, useAuth } from '../context/AuthProvider';
+
+// Makes sure the user is authenticated before accessing protected pages
+const InitialLayout = () => {
+  const { session, initialized } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!initialized) return;
+
+    // Check if the path/url is in the (auth) group
+    const inAuthGroup = segments[0] === '(auth)';
+
+    if (session && !inAuthGroup) {
+      // Redirect authenticated users to the list page
+      router.replace('/(auth)/home');
+    } else if (!session) {
+      // Redirect unauthenticated users to the login page
+      router.replace('/(public)/login');
+    }
+  }, [session, initialized]);
+
   return <Slot />;
+};
+
+// Wrap the app with the AuthProvider
+const RootLayout = () => {
+  return (
+    <AuthProvider>
+      <InitialLayout />
+    </AuthProvider>
+  );
 };
 
 export default RootLayout;
--- lib/initSupabase.tsx
+++ lib/initSupabase.tsx
@@ -1,13 +1,27 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
 import 'react-native-url-polyfill/auto';
+
+// Custom secure storage solution for the Supabase client to store the JWT
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
 
 const url = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const key = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+// initialize the supabase client
 export const supabase = createClient(url!, key!, {
   auth: {
-    storage: AsyncStorage,
+    storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
