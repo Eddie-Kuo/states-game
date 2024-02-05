48e0107c3c736d2ba785e8a05a0e8baa1ba60e9f
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-02-04 18:02:21 -0600

    add drawer label ui and icons

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,34 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-// import CustomDrawerContent from '@/components/CustomDrawerContent';
 
 export default function Layout() {
   return (
     // Required after Expo SDK 50
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer>
-        <Drawer.Screen name='home' />
-        <Drawer.Screen name='users' />
+        <Drawer.Screen
+          name='home'
+          options={{
+            drawerLabel: 'Home',
+            title: '',
+
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='users'
+          options={{
+            drawerLabel: 'Users',
+            title: '',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='person-circle' color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer>
     </GestureHandlerRootView>
   );
