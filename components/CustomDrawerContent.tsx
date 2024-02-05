82838f59d8f47816f99c167406421b5d14119bce
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-02-04 18:04:10 -0600

    init custom drawer content for drawer

---
+++ components/CustomDrawerContent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const CustomDrawerContent = () => {
+  return (
+    <View>
+      <Text>CustomDrawerContent</Text>
+    </View>
+  );
+};
+
+export default CustomDrawerContent;
+
+const styles = StyleSheet.create({});
