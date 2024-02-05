4ad7377e869414209a022820158f668d23a4bbea
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-02-05 16:46:40 -0600

    fetch list of all users

--- app/(auth)/users.tsx
+++ app/(auth)/users.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { supabase } from '@/lib/initSupabase';
+import { User } from '@supabase/supabase-js';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Users = () => {
+  const [users, setUsers] = useState<User[] | null>();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await supabase.from('profiles').select();
+
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <View>
       <Text>Users</Text>
