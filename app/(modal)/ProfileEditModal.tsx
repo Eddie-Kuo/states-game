79063cc07882d25cb5d01ea2da7bef00640d3d4a
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-02-14 17:11:17 -0600

    create custom text input fields

--- app/(modal)/ProfileEditModal.tsx
+++ app/(modal)/ProfileEditModal.tsx
@@ -1,10 +1,30 @@
+import ProfileEditInput from '@/components/ProfileEditInput';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 const ProfileEditModal = () => {
   return (
-    <View>
-      <Text>ProfileEditModal</Text>
+    <View style={{ paddingTop: 20, gap: 10 }}>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          alignSelf: 'center',
+          borderWidth: 0.2,
+        }}>
+        <Image
+          source={require('@/assets/images/placeholder.jpg')}
+          style={{
+            width: '100%',
+            height: '100%',
+            borderRadius: 50,
+          }}
+        />
+      </View>
+      <ProfileEditInput>First Name</ProfileEditInput>
+      <ProfileEditInput>Last Name</ProfileEditInput>
+      <ProfileEditInput>Username </ProfileEditInput>
     </View>
   );
 };
