1e27c2571aa5497c7fb610d07c2061b66ce5c34a
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-08 11:11:16 -0500

    refactor game list styles

--- app/(auth)/(drawer)/home.tsx
+++ app/(auth)/(drawer)/home.tsx
@@ -34,29 +34,12 @@ const Home = () => {
     <View style={styles.container}>
       <TouchableOpacity
         onPress={() => router.push('/modal')}
-        style={{
-          position: 'absolute',
-          top: 5,
-          right: 5,
-          paddingVertical: 8,
-          paddingHorizontal: 12,
-          borderWidth: 0.5,
-          margin: 4,
-          borderRadius: 25,
-        }}>
+        style={styles.editProfileButton}>
         <Text>Edit Profile</Text>
       </TouchableOpacity>
-      <View
-        style={{ marginTop: 50, width: '100%', alignItems: 'center', gap: 5 }}>
-        <Text style={{ fontSize: 25, fontWeight: '600' }}>Your Games</Text>
-        <View
-          style={{
-            width: '95%',
-            height: 1,
-            backgroundColor: 'grey',
-            opacity: 0.3,
-          }}
-        />
+      <View style={styles.gamesContainer}>
+        <Text style={styles.gamesHeader}>Your Games</Text>
+        <View style={styles.separator} />
         <FlatList
           style={{ margin: 10, height: '100%' }}
           renderItem={renderGames}
@@ -75,5 +58,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+  },
+  editProfileButton: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderWidth: 0.5,
+    margin: 4,
+    borderRadius: 25,
+  },
+  gamesContainer: {
+    marginTop: 50,
+    width: '100%',
+    alignItems: 'center',
+    gap: 5,
+  },
+  gamesHeader: { fontSize: 25, fontWeight: '600' },
+  separator: {
+    width: '95%',
+    height: 1,
+    backgroundColor: 'grey',
+    opacity: 0.3,
   },
 });
